7c46ec4bc79e23b42531fd5e250a9005b3931268
aljaffarsyah <aljaffarsyah10@gmail.com>
2022-12-22 07:36:35 +0700

    fix login register page

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/products.dart';
+import './providers/auth.dart';
+
+import './pages/home_page.dart';
+import './pages/auth_page.dart';
+import './pages/add_product_page.dart';
+import './pages/edit_product_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Products>(
+          create: (context) => Products(),
+          update: (context, auth, products) =>
+              products..updateData(auth.token, auth.userId),
+        ),
+      ],
+      builder: (context, child) => Consumer<Auth>(
+        builder: (context, auth, child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.amber,
+            accentColor: Colors.orange,
+            buttonColor: Colors.orange,
+            textTheme: TextTheme(
+              headline3: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 45.0,
+                color: Colors.black,
+              ),
+              button: TextStyle(
+                fontFamily: 'OpenSans',
+              ),
+              subtitle1: TextStyle(fontFamily: 'NotoSans'),
+              bodyText2: TextStyle(fontFamily: 'NotoSans'),
+            ),
+          ),
+          home: auth.isAuth
+              ? HomePage()
+              : FutureBuilder(
+                  future: auth.autoLogin(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Scaffold(
+                        body: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    }
+
+                    return LoginPage();
+                  },
+                ),
+          routes: {
+            AddProductPage.route: (ctx) => AddProductPage(),
+            EditProductPage.route: (ctx) => EditProductPage(),
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:autentikasi/main.dart';
+import '../providers/auth.dart';
+
+const users = const {
+  'dribbble@gmail.com': '12345',
+  'hunter@gmail.com': 'hunter',
+};
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String> _authUserSignUp(LoginData data) {
+    return Future.delayed(loginTime).then((_) async {
+      try {
+        await Provider.of<Auth>(context, listen: false)
+            .signup(data.name, data.password);
+      } catch (err) {
+        print(err);
+        return err.toString();
+      }
+      return null;
+    });
+  }
+
+  Future<String> _authUserLogin(LoginData data) {
+    return Future.delayed(loginTime).then((_) async {
+      try {
+        await Provider.of<Auth>(context, listen: false)
+            .login(data.name, data.password);
+      } catch (err) {
+        print(err);
+        return err.toString();
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'Username not exists';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+
+      title: 'RSUD Labusel',
+      // logo: 'assets/images/ecorp-lightblue.png',
+      logo: 'assets/images/logo_labusel.jpg',
+      onLogin: _authUserLogin,
+      onSignup: _authUserSignUp,
+      loginProviders: <LoginProvider>[
+        LoginProvider(
+          icon: FontAwesomeIcons.google,
+          // label: 'Google',
+          callback: () async {
+            debugPrint('start google sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop google sign in');
+            return null;
+          },
+        ),
+        LoginProvider(
+          icon: FontAwesomeIcons.facebookF,
+          // label: 'Facebook',
+          callback: () async {
+            debugPrint('start facebook sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop facebook sign in');
+            return null;
+          },
+        ),
+        LoginProvider(
+          icon: FontAwesomeIcons.linkedinIn,
+          callback: () async {
+            debugPrint('start linkdin sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop linkdin sign in');
+            return null;
+          },
+        ),
+        LoginProvider(
+          icon: FontAwesomeIcons.githubAlt,
+          callback: () async {
+            debugPrint('start github sign in');
+            await Future.delayed(loginTime);
+            debugPrint('stop github sign in');
+            return null;
+          },
+        ),
+      ],
+      onSubmitAnimationCompleted: () {
+        Provider.of<Auth>(context, listen: false).tempData();
+      },
+      onRecoverPassword: _recoverPassword,
+    );
+  }
+}
